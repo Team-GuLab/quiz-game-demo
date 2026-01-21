cd63da38af3c84618aa975b3bade5bb4843feb1f
dgh06175 <dgh06175@gmail.com>
2026-01-21 17:51:05 +0900

    옵션 레이블을 원래 위치에 고정하고 스타일 수정

--- src/components/GameBoard2D.jsx
+++ src/components/GameBoard2D.jsx
@@ -29,25 +29,46 @@ const GameBoard2D = ({ currentArea, showCorrectAnswer, correctAnswer, onAreaClic
     { index: 3, label: '4' },
   ]
 
+  // 원래 FloatingLabels 위치
+  const optionPositions = [
+    { top: '25%', left: '25%' },   // 영역 1 (좌상단)
+    { top: '25%', left: '75%' },   // 영역 2 (우상단)
+    { top: '75%', left: '25%' },   // 영역 3 (좌하단)
+    { top: '75%', left: '75%' },   // 영역 4 (우하단)
+  ]
+
   return (
-    <div className="game-board-2d">
-      {areas.map((area) => (
-        <div
-          key={area.index}
-          className={`board-area ${currentArea === area.index ? 'active' : ''}`}
-          style={{ backgroundColor: getAreaColor(area.index) }}
-          onClick={() => onAreaClick && onAreaClick(area.index)}
-        >
-          <span className="area-number">{area.label}</span>
-          {options && options[area.index] && (
-            <div className={`area-option ${getOptionStyle(area.index)}`}>
-              <span className="option-number">{area.index + 1}</span>
-              <span className="option-text">{options[area.index]}</span>
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="game-board-2d">
+        {areas.map((area) => (
+          <div
+            key={area.index}
+            className={`board-area ${currentArea === area.index ? 'active' : ''}`}
+            style={{ backgroundColor: getAreaColor(area.index) }}
+            onClick={() => onAreaClick && onAreaClick(area.index)}
+          >
+            <span className="area-number">{area.label}</span>
+          </div>
+        ))}
+      </div>
+
+      {/* 옵션 레이블 - 원래 위치에 고정 */}
+      <div className="options-overlay">
+        {options && options.map((option, index) => (
+          <div
+            key={index}
+            className={`area-option ${getOptionStyle(index)}`}
+            style={{
+              top: optionPositions[index].top,
+              left: optionPositions[index].left,
+            }}
+          >
+            <span className="option-number">{index + 1}</span>
+            <span className="option-text">{option}</span>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
